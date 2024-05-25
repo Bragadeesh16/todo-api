e131773e325850d6fded7eb0410918d1a8bbc1b9
bragadeesh <bragadeesh03294@gamil.com>
2024-05-25 14:05:55 +0530

    change

--- todo/urls.py
+++ todo/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('',views.todo_list.as_view(),name='todo-list'),
-    path('update/<int:pk>',views.todo_modify.as_view(),name='modify')
+    path('<int:pk>',views.todo_modify.as_view(),name='modify')
     
 ]
