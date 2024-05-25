f90caa62bd016d2018e7092418d07861f7967f69
bragadeesh <bragadeesh03294@gamil.com>
2024-05-25 14:16:00 +0530

    final commit

--- todo/urls.py
+++ todo/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.todo_list.as_view(),name='todo-list'),
-    path('<int:pk>',views.todo_modify.as_view(),name='modify')
+    path('todos',views.todo_list.as_view(),name='todo-list'),
+    path('todos/<int:pk>',views.todo_modify.as_view(),name='modify')
     
 ]
